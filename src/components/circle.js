c085794d064f0c5c284864450138aeb8af630630
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-11-05 18:53:32 +0530

    finally master this process :)

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
-
+const Shape = require('/shape');
 // declare class
-
+class Circle{
+    constructor(){
+        super();
+        this.color = "black";
+    }
+    calculateArea (){
+        console.log("Area of " + this.color + " circle is:")
+        return 3.14 * 2 * 2;
+    }
+}
 
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
-
+class Shape{
+    constructor(){
+        this.color = "pink";
+    }
+    drawShape(){
+        console.log("I dont have Shape!!!");
+    }
+    calculateArea(){
+        console.log("We need dimensions of the Shape");
+        return 0;
+    }
+}
 // export class using module.exports
+module.exports = Shape;
